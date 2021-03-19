1d1458f62c492a7de97a0af7da53e53fe5542fb8
jacob-tucker <jacobtucker818@gmail.com>
2021-03-19 01:42:06 -0400

    Add support for localnet wallet

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import * as fcl from "@onflow/fcl"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Use this for localnet
+// Steps to authentication:
+// Step 1) Run flow emulator start -v in a termiaml
+// Step 2) Run fcl-wallet in a second terminal (we need the .put for this part)
+fcl.config()
+  .put("challenge.handshake", "http://localhost:8701/flow/authenticate")
+
+/* For testnet
 fcl.config()
   .put("accessNode.api", "https://access-testnet.onflow.org") // connect to Flow testnet
   .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // use Blocto testnet wallet
+*/
 
 ReactDOM.render(
   <React.StrictMode>
